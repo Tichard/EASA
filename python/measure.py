67c211d398057c746be614607634a2c468e1095a
EC_TestBench <root@EC-PC2.Electrocompaniet>
2016-08-18 09:15:34 -0500

    Alpha Version :  -Minor improvements  -Sampling rate read from ADC config

--- python/measure.py
+++ python/measure.py
@@ -15,6 +15,7 @@ import analyze as fct
 
 #external import
 import sys
+import os
 import platform
 
 import scipy.signal as sig
@@ -22,6 +23,42 @@ import numpy as np
 
 from FT4222Programmer import ft4222
 from FT4222Programmer.adc088s022 import ADC088S022
+
+def init():
+	"""
+	INPUT:
+
+	OUTPUT :
+	PIN : array-like
+		list of hardware element
+
+	Init the Hardware
+	"""
+	
+	devices = ft4222.FT4222.enumerateDevices()
+
+	# checking if the FT4222 is connected
+	if not len(devices):
+		print "ERROR: Device not found !"
+		sys.exit(1)
+
+	#configure GPIO direction
+	gpio = ft4222.GPIO(devices[len(devices)-1]['locid'],
+				     [	ft4222.GPIO.GPIO_INPUT,
+					ft4222.GPIO.GPIO_OUTPUT,
+					ft4222.GPIO.GPIO_OUTPUT,
+					ft4222.GPIO.GPIO_OUTPUT ] )
+	# pin aliases
+	POWER = ft4222.GPIO.GPIO_PORT1 
+	spi0 = ft4222.FT4222(devices[0]['locid'], 0)
+	SELECT_ADC = ft4222.GPIO.GPIO_PORT3
+	ADC = ADC088S022(spi0)
+	PIN ={'power': POWER,'select_adc':SELECT_ADC,'adc':ADC}
+
+	gpio.set(POWER, True) #power on
+	gpio.set(SELECT_ADC, True) # Enable the ADC reading
+
+	return PIN
 
 
 def measure(f, n=0,boolPlot=0):
@@ -38,45 +75,22 @@ def measure(f, n=0,boolPlot=0):
 	return : integer
 		return 1 if the function has been fully executed       
 	"""
-	
-	devices = ft4222.FT4222.enumerateDevices()
 
-	# checking if the FT4222 is connected
-	if not len(devices):
-		print "ERROR: Device not found !"
-		sys.exit(1)
-
-	#configure GPIO direction
-	gpio = ft4222.GPIO(devices[len(devices)-1]['locid'],
-				     [	ft4222.GPIO.GPIO_INPUT,
-					ft4222.GPIO.GPIO_OUTPUT,
-					ft4222.GPIO.GPIO_OUTPUT,
-					ft4222.GPIO.GPIO_OUTPUT ] )
-	
-	# pin aliases
-	POWER_ON = ft4222.GPIO.GPIO_PORT1 
-	spi0 = ft4222.FT4222(devices[0]['locid'], 0)
-	SELECT_ADC = ft4222.GPIO.GPIO_PORT3
-	adc = ADC088S022(spi0)
-
-	gpio.set(POWER_ON, True) #power on
-	gpio.set(SELECT_ADC, True) # Enable the ADC reading
-
+        pin = init()
+		
 	f = min(max(f,10),30000)
 	order = min(max(n+2,2),6)
 	
-	Fs = 117000 #sampling rate 117kHz
-	df = 10 # 10Hz precision (maximum supported)
+	Fs = pin['adc'].Fs
+	df = 10 #Hz precision (maximum supported : 10Hz)
 	N = np.ceil(Fs/df) #number of samples
 	T =1.050/df #take 5% more samples than needed to avoid transient response
 
 	#recording @ <Fs>Hz sampling rate
-	data = readVoltage(adc,T)
+	data = readVoltage(pin['adc'],T)
 	end = len(data)
 	nb = int(N)
 		
-	gpio.set(POWER_ON, False) #power off
-
 	#windowing or not the response signal
 	signal = data[end-nb:end] #* sig.blackmanharris(N) #Blackman-Harris window !!!Amplitude issues!!!
 	(fourier, H, THD, SNR) = fct.analyze(signal, Fs, f, order)
@@ -93,6 +107,8 @@ def measure(f, n=0,boolPlot=0):
 		import matplotlib.pyplot as plt
 		plt.plot(fourier[0],fourier[1])
 		plt.show()
+
+	#gpio.set(pin['power'], False) #power off !! gpio nonexistant in this scope
 		
 	return 1
 
@@ -106,19 +122,24 @@ def readVoltage(ADC,T):
 	data : float
 		voltage read on the pin
 	"""
-	data = ADC.read( ADC088S022.CHANNEL_2, 0.01 )
-	print data
-		
+	data = ADC.read( ADC088S022.CHANNEL_2, T )
+
 	return data
-	
+
+def readDC(ADC):
+	data = np.array(ADC.read( ADC088S022.CHANNEL_2, 0.001 ))
+	end = int(len(data))
+	v = np.mean(data[end-100:end]) #mean of the 100 last samples
+	print v
+	return v	
+
 #-------------------------------MODULE TEST ZONE--------------------------------
 
 if __name__ == '__main__':
 	
-
 	f = 1000
-	n=1
+	n=3
        
 	measure(f,n)
-	
+	print	
 #--------------------------------------EOF--------------------------------------
